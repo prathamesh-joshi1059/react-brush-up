3011b1b9b96c8c88bf00b40bd7e81f73d709c387
prathamesh Joshi <prathamesh.joshi@zingworks.in>
2024-04-03 18:14:53 +0530

    currencyconverter project api call changed

--- 06currencyconverter/src/App.jsx
+++ 06currencyconverter/src/App.jsx
@@ -4,8 +4,8 @@ import useCurrencyInfo from "./hooks/useCurrencyInfo";
 
 function App() {
   const [amount, setAmount] = useState(0);
-  const [from, setFrom] = useState("usd");
-  const [to, setTo] = useState("inr");
+  const [from, setFrom] = useState("USD");
+  const [to, setTo] = useState("INR");
   const [convertedAmount, setConvertedAmount] = useState(0);
 
   const currencyInfo = useCurrencyInfo(from);
--- 06currencyconverter/src/hooks/useCurrencyInfo.js
+++ 06currencyconverter/src/hooks/useCurrencyInfo.js
@@ -2,11 +2,12 @@ import {useEffect, useState} from "react"
 
 
 function useCurrencyInfo(currency){
+    const apiKey = "059232e6a6eb36f1f4b399e3";
     const [data, setData] = useState({})
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`)
         .then((res) => res.json())
-        .then((res) => setData(res[currency]))
+        .then((res) => setData(res.conversion_rates))
         console.log(data);
     }, [currency])
     console.log(data);
