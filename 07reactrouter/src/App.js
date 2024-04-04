cfc96680043231d77393e31d6a0b04fb70905686
prathamesh Joshi <prathamesh.joshi@zingworks.in>
2024-04-04 12:29:05 +0530

    reactrouter project

---
+++ 07reactrouter/src/App.js
@@ -0,0 +1,13 @@
+import Header from './components/Header/Header.jsx'
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1 className='bg-purple-500 text-black p-4 m-8 text-center'>Hello Tailwind</h1>
+    <Header></Header>
+    </>
+  );
+}
+
+export default App;
